1f3168577ef589a062619035baef771b3318fbf5
Pansoymad <143574388+Pansoymad@users.noreply.github.com>
2026-01-13 12:50:47 +0800

    Add files via upload

---
+++ PansoyIPT102Solution/PansoyRepository/Interfaces/IRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Repository.Interface
+{
+    public interface IRepository<M>
+    {
+        Task<IEnumerable<M>> ObtainAllAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            );
+
+        Task<M> ObtainByIdAsync(
+            string connectionString,
+
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            );
+
+        Task<bool> CreateAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            );
+
+        Task<bool> UpdateAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            );
+
+        Task<bool> DeleteAsync(
+            string connectionString,
+            string procedureName,
+            DynamicParameters parameters = null
+            );
+    }
+}
---
+++ PansoyIPT102Solution/PansoyRepository/Repository.cs
@@ -0,0 +1,95 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Repository.Interface;
+
+namespace Repository
+{
+    public class Repository<M> : IRepository<M>
+    {
+        private SqlConnection CreateConnection(string connectionString)
+        => new SqlConnection(connectionString);
+
+        public async Task<IEnumerable<M>> ObtainAllAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            )
+        {
+            using var connection = CreateConnection(connectionString);
+            return await connection.QueryAsync<M>(
+                storedProcedureName,
+                parameters,
+                commandType: System.Data.CommandType.StoredProcedure
+                );
+        }
+
+        public async Task<M> ObtainByIdAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            )
+
+        {
+            using var connection = CreateConnection(connectionString);
+            return await connection.QueryFirstOrDefault
+                (
+                storedProcedureName,
+                parameters,
+                commandType: System.Data.CommandType.StoredProcedure
+                );
+        }
+
+        public async Task<bool> CreateAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            )
+
+        {
+            using var connection = CreateConnection(connectionString);
+            var result = await connection.ExecuteAsync(
+                storedProcedureName,
+                parameters,
+                commandType: System.Data.CommandType.StoredProcedure
+                );
+            return result > 0;
+        }
+
+        public async Task<bool> UpdateAsync(
+            string connectionString,
+            string storedProcedureName,
+            DynamicParameters parameters = null
+            )
+
+        {
+            using var connection = CreateConnection(connectionString);
+            var result = await connection.ExecuteAsync(
+                storedProcedureName,
+                parameters,
+                commandType: System.Data.CommandType.StoredProcedure
+                );
+
+            return result > 0;
+        }
+
+        public async Task<bool> DeleteAsync(
+             string connectionString,
+             string procedureName,
+             DynamicParameters parameters = null
+             )
+
+        {
+            using var connection = CreateConnection(connectionString);
+            var result = await connection.ExecuteAsync(
+                procedureName,
+                parameters,
+                commandType: System.Data.CommandType.StoredProcedure
+                );
+
+            return result > 0;
+        }
+
+    }
+
+}
+
